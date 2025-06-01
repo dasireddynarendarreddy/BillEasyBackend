a5291a28025a3dee1545065ec0dee88f52a8bee7
dasireddynarendarreddy <narendardasireddy4@gmail.com>
2025-06-01 12:52:15 +0530

    updated

--- index.js
+++ index.js
@@ -29,6 +29,4 @@ mongoose.connect(process.env.MONGO_URI)
         app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
     })
     .catch((err) => console.error('MongoDB connection error:', err));
-app.listen(PORT,()=>{
-  console.log("app running on",PORT)
-})
+
